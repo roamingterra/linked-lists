b6372245adce728caa00283ffb947bb1537fab26
roamingterra <danielballerini1@gmail.com>
2023-04-07 13:50:01 -0400

    Create Node factory function
    This was also a refresher for me with regards to scope. I initially
    created the methods using the arrow function syntax, however, this
    creates lexical scope. This means that when the method is called, the
    scope is inherited from the context that the method was called. Instead,
    I decided to use the regular function syntax for my methods, and use the
    "this" keyword to make it so that the context of the properties being
    changed is referring to the properties of the created object.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+// Factory function which represents the full list
+function LinkedList(value, index) {
+  return {};
+}
+
+// Factory function for creating new nodes, complete with methods to update properties
+function Node(value = null, nextNode = null) {
+  return {
+    data: value,
+    next: nextNode,
+    setValue: function (newData) {
+      this.data = newData;
+    },
+    setNext: function (newNextNode) {
+      this.next = newNextNode;
+    },
+  };
+}
+
+// const n1 = Node(200);
+// const n2 = Node(500, n1);
+// n2.setValue(400);
+// n2.setNext(null);
+// console.log(n1);
+// console.log(n2);
