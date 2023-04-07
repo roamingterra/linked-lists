d7a14d5756f21101aed76b491a06eb4c45c39b7d
roamingterra <danielballerini1@gmail.com>
2023-04-07 14:41:55 -0400

    Create LinkedList factory function
    I added an append methods to this LinkedList factory function which
    creates a new node (with help from the Node factory function), and
    appends it to the end of the list.

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // Factory function which represents the full list
-function LinkedList(value, index) {
-  return {};
+function LinkedList() {
+  return {
+    head: null,
+    append: function (value) {
+      const newNode = Node(value);
+
+      // If linked list is empty, make new node the head
+      if (!this.head) {
+        this.head = newNode;
+      } else {
+        // Find last node in the list
+        let lastNode = this.head;
+        // if head node has a pointer pointing to a next node, enter while loop
+        while (lastNode.next) {
+          // Set new last node
+          lastNode = lastNode.next;
+        }
+
+        // Set the new node as the next node of the last node
+        lastNode.setNext(newNode);
+      }
+    },
+  };
 }
 
 // Factory function for creating new nodes, complete with methods to update properties
@@ -17,9 +38,12 @@ function Node(value = null, nextNode = null) {
   };
 }
 
-// const n1 = Node(200);
-// const n2 = Node(500, n1);
-// n2.setValue(400);
-// n2.setNext(null);
-// console.log(n1);
-// console.log(n2);
+const myList = LinkedList();
+
+myList.append(10);
+myList.append(20);
+myList.append(30);
+
+console.log(myList);
+
+// Next, I will create new LinkedList methods to do more things to the linked list
